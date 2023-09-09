e1410214db84444567d96488a5b3a319a05cbc58
anthonydhudson <ahudson@icstars.org>
2023-09-09 00:11:36 -0500

    index
    "index.jsx view rendered at pokemon route"

--- pokemonapp/server.js
+++ pokemonapp/server.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const React = require('react');
+const ReactDOMServer = require('react-dom/server');
+const Index = require('./views/Index');
+
+app.get('/pokemon', (req, res) => {
+  const indexHtml = ReactDOMServer.renderToString(
+    <Index pokemon={pokemon} />
+  );
+  res.send(indexHtml);
+});
 
 const pokemon = require('./models/pokemon');
+
+app.get('/pokemon/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedPokemon = pokemon[id];
+  
+    res.send(`You selected: ${selectedPokemon.name}`);
+  });  
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
---
+++ pokemonapp/views/Index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Index = ({ pokemon }) => {
+  return (
+    <div>
+      <h1>See All The Pokemon!</h1>
+      <ul>
+        {pokemon.map((p, index) => (
+          <li key={index}>
+            <a href={`/pokemon/${index}`}>{p.name}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Index;
